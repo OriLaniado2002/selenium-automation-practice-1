91d1d2bb9c3d277becd6eda4dc88875ecc768652
OriLaniado2002 <Laniadori01@gmail.com>
2025-06-03 15:27:05 +0300

    initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace automation_solo_practice_1;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
